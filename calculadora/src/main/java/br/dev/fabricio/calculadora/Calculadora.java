904f93a23015587257d9735d10b22ce2f847fdd0
Fabricio Guimaraes de Lima <fabricio@lopesecia.com.br>
2026-03-17 08:14:27 -0300

    Testes unitario calculadora

--- calculadora/src/main/java/br/dev/fabricio/calculadora/Calculadora.java
+++ calculadora/src/main/java/br/dev/fabricio/calculadora/Calculadora.java
@@ -1,9 +1,22 @@
 package br.dev.fabricio.calculadora;
 
-public class Calculadora {
+public final class Calculadora {
 
-  public static double somar(double a, double b){
-    return a + b;
+  private Calculadora(){}
+
+  public static double somar(double valor1, double valor2){
+    return valor1 + valor2;
   }
 
+  public static double subtrair(double valor1, double valor2) {
+    return valor1 - valor2;
+  }
+
+  public static double multiplicar(double valor1, double valor2) {
+    return valor1 * valor2;
+  }
+
+  public static double dividir(double valor1, double valor2) {
+    return valor1 / valor2;
+  }
 }
--- calculadora/src/test/java/br/dev/fabricio/calculadora/CalculadoraTeste.java
+++ calculadora/src/test/java/br/dev/fabricio/calculadora/CalculadoraTeste.java
@@ -3,13 +3,43 @@ package br.dev.fabricio.calculadora;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class CalculadoraTeste {
-
+class CalculadoraTeste {
 
   @Test
   void soma(){
     double resultado = Calculadora.somar(2,2);
     Assertions.assertEquals(4, resultado);
   }
+
+  @Test
+  void subtracao(){
+    double resultado = Calculadora.subtrair(10,5);
+    Assertions.assertEquals(5, resultado);
+  }
+
+  @Test
+  void multiplicacao(){
+    double resultado = Calculadora.multiplicar(2,2);
+    Assertions.assertEquals(4, resultado);
+  }
+
+  @Test
+  void divisao(){
+    double resultado = Calculadora.dividir(10,2);
+    Assertions.assertEquals(5, resultado);
+  }
+
+  @Test
+  void divisaoDoisValoresZeroResultadoEmNaN(){
+    double resultado = Calculadora.dividir(0,0);
+    Assertions.assertEquals(Double.NaN, resultado);
+  }
+
+  @Test
+  void divisaoPorZeroResultandoEmPositivoInfinito(){
+    double resultado = Calculadora.dividir(10,0);
+    Assertions.assertEquals(Double.POSITIVE_INFINITY, resultado);
+  }
+
 
 }
