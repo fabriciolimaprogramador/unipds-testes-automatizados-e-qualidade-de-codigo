f85e7dd0be8fae39dd4109919304c683983a0102
Fabricio Guimaraes de Lima <fabricio@lopesecia.com.br>
2026-03-18 23:03:04 -0300

    Modulo 05 Video 06 Testes sem mock

--- modulo9-payment-system-part-4-main/src/test/java/com/eliasnogueira/paymentservice/unit/PaymentLimitValidatorTest.java
+++ modulo9-payment-system-part-4-main/src/test/java/com/eliasnogueira/paymentservice/unit/PaymentLimitValidatorTest.java
@@ -1,5 +1,6 @@
 package com.eliasnogueira.paymentservice.unit;
 
+import com.eliasnogueira.paymentservice.exceptions.PaymentLimitException;
 import com.eliasnogueira.paymentservice.validator.PaymentLimitValidator;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -26,5 +27,32 @@ public class PaymentLimitValidatorTest {
     boolean isWithinLimit = PaymentLimitValidator.isWithinLimit(null);
     assertThat(isWithinLimit).isFalse();
   }
+
+  @Test
+  void shouldNotAcceptAmountGreaterThanLimit(){
+    boolean isWithinLimit = PaymentLimitValidator.isWithinLimit(new BigDecimal("3000"));
+
+    assertThat(isWithinLimit).isFalse();
+  }
+
+  @Test
+  void shouldNotAcceptZeroAmount(){
+
+    assertThatThrownBy(() -> PaymentLimitValidator.isWithinLimit(BigDecimal.ZERO))
+            .isInstanceOf(PaymentLimitException.class)
+            .hasMessage("Amount must be greater than zero");
+
+  }
+
+  @Test
+  void shouldNotAcceptNegativeAmount(){
+
+    assertThatThrownBy(() -> PaymentLimitValidator.isWithinLimit(new BigDecimal("-5.00")))
+            .isInstanceOf(PaymentLimitException.class)
+            .hasMessage("Amount must be greater than zero");
+
+  }
+
+
 
 }
